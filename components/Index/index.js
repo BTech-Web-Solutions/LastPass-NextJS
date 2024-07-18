0963b4485cc5e48fe2dba2d3c4ec05106012230a
brodrigues0ll <bernardorg99@hotmail.com>
2024-07-18 00:26:28 -0300

    set inputs test

--- components/Index/index.js
+++ components/Index/index.js
@@ -1,22 +1,40 @@
-import { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import AppContext from "../../contexts/appContext";
 
 export default function Index({ navigateToPage }) {
   const { currentUrl, setCurrentUrl } = useContext(AppContext);
+  const [ok, setOk] = useState(false);
 
   useEffect(() => {
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       const activeTab = tabs[0];
       setCurrentUrl(activeTab.url);
+
+      const setEmail = "emailtest@test.com";
+      const setPass = "PASSWORDTEST.COM";
+
+      chrome.tabs.sendMessage(
+        activeTab.id,
+        {
+          action: "setFormValues",
+          email: setEmail,
+          password: setPass,
+        },
+        (response) => {
+          if (response && response.success) {
+            setOk(true);
+          }
+        }
+      );
     });
-  }, []);
+  }, [setCurrentUrl]);
 
   return (
     <div>
       <main>
         <h1 className="font-semibold text-3xl">Current URL:</h1>
-
         <p className="font-semibold text-xl">{currentUrl}</p>
+        {ok && <h1 className="font-semibold text-2xl text-red-600">OK</h1>}
         <p onClick={() => navigateToPage("new")}>{"Go to New Page >"}</p>
       </main>
     </div>
--- out/inject.js
+++ out/inject.js
@@ -1,1 +1,9 @@
-console.log("Message from inject.js")
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "setFormValues") {
+    const emailInput = document.getElementById("email");
+    const passInput = document.getElementById("pass");
+    if (emailInput) emailInput.value = request.email;
+    if (passInput) passInput.value = request.password;
+    sendResponse({ success: true });
+  }
+});
